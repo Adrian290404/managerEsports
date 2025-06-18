a1e4acbfb9b3742e90f92b583edf3effd52b9864
Adrian290404 <adrianmg2004.29@gmail.com>
2025-06-18 12:06:10 +0200

    testing bbdd connection

--- src/pages/teamSelectPage.tsx
+++ src/pages/teamSelectPage.tsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Background, Title, AddButton, CardContainer } from "../styles/teamSelectStyles";
 import { Link } from "react-router-dom";
 import { TeamCard } from "../components/others/teamCard";
 import vortex from "../assets/vortex.png"
 import vitality from "../assets/vitality.png"
 
+// interface TestItem {
+//   id: number;
+//   texto: string;
+//   numero: number;
+// }
+
 export const TeamSelectPage: React.FC = () => {
+
+    // const [data, setData] = useState<TestItem[]>([]);
+
+    // useEffect(() => {
+    //     fetch('http://localhost/managerEsportsBack/getTestData.php')
+    //         .then(response => response.json())
+    //         .then(result => setData(result))
+    //         .catch(error => console.error("Error al obtener datos:", error));
+    // }, []);
 
     const teams = ["vortex", "vitality"];
 
@@ -21,5 +36,24 @@ export const TeamSelectPage: React.FC = () => {
         <Link to="/team-form">
             <AddButton>+</AddButton>
         </Link>
+        {/* <h2>Datos de la tabla test</h2>
+        <table>
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Texto</th>
+                <th>Numero</th>
+            </tr>
+            </thead>
+            <tbody>
+                {data.map((item) => (
+                    <tr key={item.id}>
+                    <td>{item.id}</td>
+                    <td>{item.texto}</td>
+                    <td>{item.numero}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table> */}
     </Background>
 };
