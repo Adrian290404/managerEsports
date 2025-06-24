3300164366a892671624ef771fcc86e37af1de23
Adrian290404 <adrianmg2004.29@gmail.com>
2025-06-24 19:55:04 +0200

    show teams on select page

--- src/pages/teamSelectPage.tsx
+++ src/pages/teamSelectPage.tsx
@@ -1,14 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Background, Title, AddButton, CardContainer } from "../styles/teamSelectStyles";
 import { Link } from "react-router-dom";
 import { TeamCard } from "../components/others/teamCard";
-import vortex from "../assets/vortex.png"
-import vitality from "../assets/vitality.png"
 
 export const TeamSelectPage: React.FC = () => {
+    const [teams, setTeams] = useState<any[]>([]);
 
     useEffect(() => {
-
         const seeder = localStorage.getItem("seeder");
 
         if (seeder !== "true") {
@@ -19,21 +17,31 @@ export const TeamSelectPage: React.FC = () => {
             localStorage.setItem("seeder", "true");
         }
 
+        fetch("http://localhost/managerEsportsBack/getTeams.php")
+            .then((res) => res.json())
+            .then((data) => setTeams(data))
+            .catch((err) => console.error("Error al cargar equipos:", err));
     }, []);
 
-    const teams = []
-
-    return <Background>
-        <Title>{teams.length != 0 ? "Selecciona un equipo" : "Aún no hay equipos registrados"}</Title>
-        {
-            teams.length != 0 && (<CardContainer>
-                    {/* <TeamCard image={vortex} id={1}></TeamCard>
-                    <TeamCard image={vitality} id={2}></TeamCard> */}
-                </CardContainer>
-            )
-        }
-        <Link to="/team-form">
-            <AddButton>+</AddButton>
-        </Link>
-    </Background>
+    return (
+        <Background>
+            <Title>{teams.length !== 0 ? "Selecciona un equipo" : "Aún no hay equipos registrados"}</Title>
+            {
+                teams.length !== 0 && (
+                    <CardContainer>
+                        {teams.map(team => (
+                            <TeamCard
+                                key={team.name}
+                                id={team.id}
+                                image={team.image}
+                            />
+                        ))}
+                    </CardContainer>
+                )
+            }
+            <Link to="/team-form">
+                <AddButton>+</AddButton>
+            </Link>
+        </Background>
+    );
 };
